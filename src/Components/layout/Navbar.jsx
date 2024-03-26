f45a2d354a15403a157a15b43f68ee6caa755b1a
Raul Casabianca <casabianca.raul@gmail.com>
2024-03-25 21:38:22 -0400

    estresado x2

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
-}
+    <>
+      <nav>
+        {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contacto</Link>
+          <Link to="/detailhome">Detalle</Link>
+          <Link to="/favs">Destacados</Link>
+          {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
 
-export default Navbar
+          <button>Change theme</button>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import Card from '../common/Card'
+import React, { useEffect, useState } from 'react';
+import Card from '../common/Card';
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
 const Home = () => {
   const [dentistas, setDentistas] = useState([]);
 
   //Llamamos a la API
-
-
   useEffect(() => {
     axios
       .get ("https://jsonplaceholder.typicode.com/users")
@@ -19,8 +16,8 @@ const Home = () => {
 
   return (
     <main className="" >
+      {/* Aqui deberias renderizar las cards */}
       <h1>Home</h1>
-        {/* Aqui deberias renderizar las cards */}
 
       <div className='card-grid'>
         {dentistas.map((dentista) => {
